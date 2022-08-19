7a44587ec58dc1285508f1a7d7d02f5b75891375
Alexander Lazar <alexandres.lazar@gmail.com>
2022-08-19 13:53:03 -0700

    Update _all_.py

--- _all_.py
+++ _all_.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python3
 
 # system ----
-import os
-import sys
-import numpy as np
-import scipy.integrate as integrate
 import warnings
 warnings.filterwarnings("ignore", category=RuntimeWarning)
 warnings.filterwarnings("ignore", category=np.VisibleDeprecationWarning)
@@ -15,6 +11,6 @@ import dehnen
 
 class Profiles(object):
 
-    def __init__(self, distribution: str ='isotropic', *args, **kwargs) -> None:
+    def __init__(self, distr: str ='isotropic', *args, **kwargs) -> None:
         self.hernquist = hernquist.Model( *args, **kwargs)
         self.dehnen = dehnen.Model(*args, **kwargs)
